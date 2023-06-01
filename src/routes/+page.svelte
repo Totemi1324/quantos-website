e34e7edf2634530b716c8f87db841310de7b5139
Totemi1324 <nemestomi2@gmail.com>
2023-06-02 01:08:52 +0200

    Add three.js configuration

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,77 @@
-<script>
-    let title = "Quantos";
+<script lang="ts">
+    import { browser } from "$app/environment";
+    import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+    import * as THREE from 'three';
 
-    const updateTitle = () => {
-        title = "Quantum computing made easy."
+    if (browser) {
+        let camera : THREE.PerspectiveCamera;
+        let scene : THREE.Scene;
+        let renderer : THREE.WebGLRenderer;
+        let loader : GLTFLoader;
+        let smartphone : THREE.Group;
+        let directionalLight : THREE.DirectionalLight;
+        let ambientLight : THREE.AmbientLight;
+
+        const init = () => {
+            scene = new THREE.Scene();
+            scene.background = null;
+            camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
+            camera.position.setZ(30);
+
+
+            renderer = new THREE.WebGLRenderer({
+                canvas: document.querySelector('#smartphone') as HTMLCanvasElement,
+                alpha: true
+            });
+            renderer.setPixelRatio(window.devicePixelRatio);
+            renderer.setSize(500, 500);
+
+            ambientLight = new THREE.AmbientLight(0xcccccc);
+            scene.add(ambientLight);
+			
+            directionalLight = new THREE.DirectionalLight(0xffffff);
+            directionalLight.position.set(0, 1, 1).normalize();
+            scene.add(directionalLight);
+
+            loader = new GLTFLoader();
+            loader.load('/smartphone.glb', function (gltf) {
+                smartphone = gltf.scene;
+                smartphone.scale.set(2, 2, 2);
+                smartphone.position.set(0, 0, 0);
+                scene.add(smartphone);
+            }, undefined, function (error) {
+                console.error(error);
+            });
+        }
+
+        const render = () => {
+            renderer.render(scene, camera);
+        }
+
+        const animate = () => {
+            requestAnimationFrame(animate);
+
+            if (smartphone) {
+                smartphone.rotation.x += 0.005;
+                smartphone.rotation.y += 0.005;
+            }
+
+            render();
+        }
+
+        init();
+        animate();
     }
 </script>
 
-<div class="index">
-    <h2>{title}</h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi impedit tenetur mollitia commodi ad excepturi similique, cumque consequatur ducimus alias rerum adipisci eaque voluptas magnam iste saepe architecto. Provident odio optio iste! Vitae dolores consectetur ratione, provident, magni similique soluta, at aspernatur rerum a voluptatem! Fugiat minima possimus quae! Doloremque laborum laudantium impedit. Quo voluptatibus rerum deserunt magnam ullam quibusdam!</p>
-
-    <button on:click={updateTitle}>Change title</button>
-    <input type="text" bind:value={title}>
+<div id="background">
+    <canvas id="smartphone"></canvas>
 </div>
 
 <style>
-    .index {
-        text-align: center;
-        display: block;
-        margin: 20px auto;
+    #smartphone {
+        position: absolute;
+        top: 0;
+        left: 0;
     }
 </style>
