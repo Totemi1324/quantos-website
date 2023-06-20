69ff9d968d425ea871b83af9fb86789a6f43c1ba
Tamas Nemes <nemestomi2@gmail.com>
2023-06-20 17:46:00 +0200

    Relocate data

--- src/components/SubscribeForm.svelte
+++ src/components/SubscribeForm.svelte
@@ -11,8 +11,20 @@
     import { initializeApp } from 'firebase/app';
     import { getFirestore } from 'firebase/firestore';
 
-    import { firebaseConfig  } from '../lib/firebase';
-    import type { Subscriber } from '../lib/types';
+    const firebaseConfig = {
+        apiKey: 'AIzaSyB98oT0lmcKov_YPxF99mTLqVt6FPDUI0s',
+        authDomain: 'quantos-website.firebaseapp.com',
+        projectId: 'quantos-website',
+        storageBucket: 'quantos-website.appspot.com',
+        messagingSenderId: '465073767089',
+        appId: '1:465073767089:web:23254e7d37fe63a5089890',
+        measurementId: 'G-9MN1ZPV5EB'
+    };
+
+    interface Subscriber {
+        firstName: string;
+        email: string;
+    }
 
     let firstName: string = '';
     let email: string = '';
