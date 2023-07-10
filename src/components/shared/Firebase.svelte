f11b1db06e829d01e2ce15ca25fef17f71a87da1
Tamas Nemes <nemestomi2@gmail.com>
2023-07-10 11:08:41 +0200

    Edit gitignore

---
+++ src/components/shared/Firebase.svelte
@@ -0,0 +1,11 @@
+<script lang="ts" context="module">
+    export const firebaseConfig = {
+        apiKey: 'AIzaSyB98oT0lmcKov_YPxF99mTLqVt6FPDUI0s',
+        authDomain: 'quantos-website.firebaseapp.com',
+        projectId: 'quantos-website',
+        storageBucket: 'quantos-website.appspot.com',
+        messagingSenderId: '465073767089',
+        appId: '1:465073767089:web:23254e7d37fe63a5089890',
+        measurementId: 'G-9MN1ZPV5EB'
+    };
+</script>
